684f2b81e6c350994c7eabd644c01faf9cc13b72
kyucchoi <111959522+anjdfk111@users.noreply.github.com>
2024-09-19 16:23:27 +0900

    Revert "[FE]feat:  vue.js 초안 추가"
    This reverts commit 18f8bcccecc26845670b703fa1229d744ffeeceb.

--- Development/FE/muno-muno/src/components/CommunityPage.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <h1>Community Page</h1>
-  </div>
-</template>
-
-<script></script>
--- Development/FE/muno-muno/src/components/ProductPage.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <h1>Product Page</h1>
-  </div>
-</template>
-
-<script></script>
