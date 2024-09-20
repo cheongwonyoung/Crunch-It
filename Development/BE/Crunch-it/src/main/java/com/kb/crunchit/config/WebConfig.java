0352f45aabdb67624a14a3a30b0d587c236f428d
Sunjin <anjdfk111@gmail.com>
2024-09-20 17:49:47 +0900

    [FE]Chore: vue.config.js, package.json, package-lock.json 수정 [BE]Chore: WebConfig cors 코드 추가

--- Development/BE/Crunch-it/src/main/java/com/kb/crunchit/config/WebConfig.java
+++ Development/BE/Crunch-it/src/main/java/com/kb/crunchit/config/WebConfig.java
@@ -14,8 +14,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
 
+//    @Override
+//    public void configureViewResolvers(ViewResolverRegistry registry) {
+//        registry.jsp("WEB-INF" , ".jsp");
+//    }
+
     @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        registry.jsp("WEB-INF" , ".jsp");
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
+                .allowedOrigins("http://localhost:8080") // 허용할 도메인 설정
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메소드
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(true); // 쿠키 허용
     }
 }
--- Development/FE/muno-muno/vue.config.js
+++ Development/FE/muno-muno/vue.config.js
@@ -1,4 +1,16 @@
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    host: 'localhost',  // 호스트를 0.0.0.0 대신 localhost로 설정
+    port: 8082,          // 포트를 8081에서 8082로 변경
+    // 필요 시 자동으로 열리게 설정
+    open: true
+  }
 })
