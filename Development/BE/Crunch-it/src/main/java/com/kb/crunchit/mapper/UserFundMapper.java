737704606f7e8bd3497db69192dc4736183cfbbd
NaGU <kwna0729@naver.com>
2024-09-24 17:43:15 +0900

    [BE][Feat]: Temp commit

--- Development/BE/Crunch-it/src/main/java/com/kb/crunchit/mapper/UserFundMapper.java
+++ Development/BE/Crunch-it/src/main/java/com/kb/crunchit/mapper/UserFundMapper.java
@@ -8,10 +8,6 @@ import java.util.List;
 
 @Mapper
 public interface UserFundMapper {
-
-    // 펀드 수익률이 가장 높은 user_id 조회
-    Integer findTopProfitUserIdByType(@Param("type") String type);
-
-    // 특정 user_id가 보유한 펀드 조회
-    List<UserFundResponseDTO> selectFundsByUserId(@Param("userId") int userId);
+    // 펀드 상위 3개 조회 메서드
+    List<UserFundResponseDTO> findTop3FundsByUserId(int userId);
 }
