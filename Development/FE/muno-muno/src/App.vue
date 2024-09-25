7dee70a8758c50810de793e3336e3bcd51932540
gimeus <gimeus@naver.com>
2024-09-25 17:24:08 +0900

    feat: tab bar 수정

--- Development/FE/muno-muno/src/App.vue
+++ Development/FE/muno-muno/src/App.vue
@@ -19,4 +19,40 @@ export default {
 body {
   font-family: 'Pretendard', sans-serif;
 }
+
+html,
+body {
+  width: 375px;
+  height: 812px;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+
+#app {
+  width: 100%;
+  height: 100%;
+}
+
+:root {
+  --gr10: #1f2226;
+  --gr20: #292d33;
+  --gr30: #383e47;
+  --gr40: #616b79;
+  --gr50: #8892a0;
+  --gr60: #d6dae0;
+  --gr70: #edeff2;
+  --gr80: #f5f6f7;
+  --gr90: #fbfbfc;
+  --gr100: #ffffff;
+  --p10: #3e8aff;
+  --p20: #5398ff;
+  --p30: #97c0fe;
+  --p40: #99c1ff;
+  --p50: #b2d1ff;
+  --p60: #b2d1ff;
+  --p70: #cce0ff;
+  --p80: #ebf3ff;
+  --p90: #f0f6ff;
+}
 </style>
