13d7a5cb9dc3a35242ea2176e43ddf03e608ec84
wonyoung <wjddnjsdud0601@gmail.com>
2024-09-27 15:42:24 +0900

    [Fix]: config 등록을 통한 websocket cors 해결

--- Development/BE/Crunch-it/src/main/java/com/kb/crunchit/config/WebAppInitializer.java
+++ Development/BE/Crunch-it/src/main/java/com/kb/crunchit/config/WebAppInitializer.java
@@ -19,7 +19,7 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[] {AppConfig.class, SecurityConfig.class, RedisConfig.class , EmailConfig.class};
+        return new Class[] {AppConfig.class, SecurityConfig.class, RedisConfig.class , EmailConfig.class, WebSocketConfig.class};
     }
 
     @Override
