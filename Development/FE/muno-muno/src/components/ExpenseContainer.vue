c236437c70f27fccae8fbcbd2d171bfd90fb53cd
gimeus <gimeus@naver.com>
2024-10-01 16:30:30 +0900

    feat: 홈 작업 완료(모델링 제외)

---
+++ Development/FE/muno-muno/src/components/ExpenseContainer.vue
@@ -0,0 +1,77 @@
+<template>
+  <section class="expense-section">
+    <div class="expense-container">
+      <div class="expense-box">
+        <!-- 지출금액 -->
+        <p class="subtitle">지출금액</p>
+        <!-- 금액 -->
+        <h1 class="amount">{{ formattedExpenseAmount }}원</h1>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    expenseAmount: {
+      type: Number,
+      required: true,
+    },
+  },
+  computed: {
+    formattedExpenseAmount() {
+      return this.expenseAmount.toLocaleString(); // 숫자를 100,000 형식으로 변환
+    },
+  },
+};
+</script>
+
+<style scoped>
+.expense-section {
+  background-color: var(--p10);
+  position: fixed;
+  bottom: 86px;
+  left: 0;
+  width: 100%;
+  height: 112px;
+  border-radius: 20px 20px 0 0;
+  display: flex;
+  justify-content: center;
+}
+
+.expense-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+
+.expense-box {
+  width: calc(100% - 40px);
+  max-width: 335px;
+  height: 64px;
+  border-radius: 12px;
+  background: rgba(255, 255, 255, 0.2);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 24px;
+  box-sizing: border-box;
+  margin: 24px 0;
+  border: 0.8px solid rgba(255, 255, 255, 0.1);
+}
+
+.subtitle {
+  color: var(--gr100);
+  font-size: 16px;
+  font-weight: 400;
+}
+
+.amount {
+  color: var(--gr100);
+  font-size: 24px;
+  font-weight: 400;
+  text-align: right;
+}
+</style>
---
+++ Development/FE/muno-muno/src/components/OctopusButton.vue
@@ -0,0 +1,67 @@
+<template>
+  <button class="octopus-button" @click="selectOctopus">
+    <div class="octopus-box">
+      <img :src="buttonImage" :alt="name" class="octopus-icon" />
+    </div>
+    <p class="octopus-text">{{ name }}</p>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'OctopusButton',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    buttonImage: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    selectOctopus() {
+      this.$emit('select');
+    },
+  },
+};
+</script>
+
+<style scoped>
+.octopus-button {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.octopus-box {
+  width: 40px;
+  height: 40px;
+  flex-shrink: 0;
+  background-color: var(--gr100);
+  border-radius: 6px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  filter: drop-shadow(0px 0px 2px rgba(0, 0, 0, 0.08));
+}
+
+.octopus-icon {
+  width: 38px;
+  height: 38px;
+  object-fit: cover;
+}
+
+.octopus-text {
+  color: var(--gr50);
+  text-align: center;
+  font-size: 11px;
+  font-weight: 600;
+  line-height: 100%;
+  margin: 6px 0 0 0;
+}
+</style>
