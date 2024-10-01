bf5c771c1f5854d2f83c2c9d1c8c4974b2eaf558
gimeus <gimeus@naver.com>
2024-10-01 17:48:19 +0900

    feat: header 컴포넌트 수정

--- Development/FE/muno-muno/src/pages/SignUpPage.vue
+++ Development/FE/muno-muno/src/pages/SignUpPage.vue
@@ -1,36 +1,36 @@
 <template lang="">
-    <div class="container">
-        <!-- <HeaderComponent title="회원가입" @close="handleClose" /> -->
-        <HeaderBackComponent title="회원가입" @back="handleClose" />
-    </div>
+  <div class="container">
+    <!-- <HeaderComponent title="회원가입" @close="handleClose" /> -->
+    <HeaderB title="회원가입" @back="handleClose" />
+  </div>
 </template>
 <script>
-    import apiClient from "@/axios";
-    // import HeaderComponent from "@/components/HeaderComponent.vue";
-    import HeaderBackComponent from "@/components/HeaderBackComponent.vue";
-    export default {
-        components: {
-            // HeaderComponent,
-            HeaderBackComponent,
-        },
-        data() {},
-        methods: {
-            handleClose() {
-                this.$router.push({ name: "Login" });
-            },
-            signUp() {
-                apiClient;
-            },
-        },
-    };
+import apiClient from '@/axios';
+// import HeaderComponent from "@/components/HeaderComponent.vue";
+import HeaderB from '@/components/HeaderB.vue';
+export default {
+  components: {
+    // HeaderComponent,
+    HeaderB,
+  },
+  data() {},
+  methods: {
+    handleClose() {
+      this.$router.push({ name: 'Login' });
+    },
+    signUp() {
+      apiClient;
+    },
+  },
+};
 </script>
 <style scoped>
-    .container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        height: 100vh;
-        box-sizing: border-box;
-    }
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  box-sizing: border-box;
+}
 </style>
--- Development/FE/muno-muno/src/router/index.js
+++ Development/FE/muno-muno/src/router/index.js
@@ -1,52 +1,52 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeP from "@/pages/Home.vue";
-import ProductP from "@/pages/Product.vue";
-import PropertyP from "@/pages/Property.vue";
-import CommunityP from "@/pages/Community.vue";
-import LoginP from "@/pages/LoginPage.vue";
-import SignupP from "@/pages/SignUpPage.vue";
-import apiClient from "@/axios";
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeP from '@/pages/Home.vue';
+import ProductP from '@/pages/Product.vue';
+import PropertyP from '@/pages/Property.vue';
+import CommunityP from '@/pages/Community.vue';
+import LoginP from '@/pages/LoginPage.vue';
+import SignupP from '@/pages/SignUpPage.vue';
+// import apiClient from '@/axios';
 
 const routes = [
-    { path: "/", name: "Home", component: HomeP },
-    { path: "/product", name: "Product", component: ProductP },
-    { path: "/property", name: "Property", component: PropertyP },
-    { path: "/community", name: "Community", component: CommunityP },
-    { path: "/login", name: "Login", component: LoginP },
-    { path: "/signup", name: "SignUp", component: SignupP },
+  { path: '/', name: 'Home', component: HomeP },
+  { path: '/product', name: 'Product', component: ProductP },
+  { path: '/property', name: 'Property', component: PropertyP },
+  { path: '/community', name: 'Community', component: CommunityP },
+  { path: '/login', name: 'Login', component: LoginP },
+  { path: '/signup', name: 'SignUp', component: SignupP },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 });
 
-const navigationRoutes = ["Login", "SignUp"];
+// const navigationRoutes = ['Login', 'SignUp'];
 
-router.beforeEach((to, from, next) => {
-    const token = localStorage.getItem("JwtToken"); // 토큰을 localStorage에서 가져옴
+// router.beforeEach((to, from, next) => {
+//     const token = localStorage.getItem("JwtToken"); // 토큰을 localStorage에서 가져옴
 
-    if (!navigationRoutes.includes(to.name)) {
-        if (!token) {
-            next({ name: "Login" });
-            return;
-        }
-        apiClient
-            .get("/auth/authenticate")
-            .then((res) => {
-                if (res.status == 200) next();
-                else {
-                    next({ name: "Login" });
-                }
-            })
-            .catch((err) => {
-                if (err.status == 401) alert("로그인이 만료되었습니다. 다시 로그인 해주세요.");
-                console.log(err);
-                next({ name: "Login" });
-            });
-    } else {
-        next();
-    }
-});
+//     if (!navigationRoutes.includes(to.name)) {
+//         if (!token) {
+//             next({ name: "Login" });
+//             return;
+//         }
+//         apiClient
+//             .get("/auth/authenticate")
+//             .then((res) => {
+//                 if (res.status == 200) next();
+//                 else {
+//                     next({ name: "Login" });
+//                 }
+//             })
+//             .catch((err) => {
+//                 if (err.status == 401) alert("로그인이 만료되었습니다. 다시 로그인 해주세요.");
+//                 console.log(err);
+//                 next({ name: "Login" });
+//             });
+//     } else {
+//         next();
+//     }
+// });
 
 export default router;
