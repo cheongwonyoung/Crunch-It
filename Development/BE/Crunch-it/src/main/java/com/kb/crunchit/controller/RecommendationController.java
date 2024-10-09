2e40f8eb72d4ba565cd081605d177d31d0262f86
wonyoung <wjddnjsdud0601@gmail.com>
2024-10-09 22:12:38 +0900

    [Feat]: 한국투자증권 토큰 발급

--- Development/BE/Crunch-it/src/main/java/com/kb/crunchit/controller/RecommendationController.java
+++ Development/BE/Crunch-it/src/main/java/com/kb/crunchit/controller/RecommendationController.java
@@ -3,7 +3,7 @@ package com.kb.crunchit.controller;
 import com.kb.crunchit.dto.response.UserBondResponseDTO;
 import com.kb.crunchit.dto.response.UserFundResponseDTO;
 import com.kb.crunchit.dto.response.UserStockResponseDTO;
-import com.kb.crunchit.service.RecommendationService;
+import com.kb.crunchit.service.RecommendationTopService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,15 +17,15 @@ import java.util.Optional;
 public class RecommendationController {
 
     @Autowired
-    private RecommendationService recommendationService;
+    private RecommendationTopService recommendationTopService;
 
     // 주식 수익률이 가장 높은 사용자의 상위 3개 주식 추천
     @GetMapping("/top-stocks")
     public List<UserStockResponseDTO> recommendTopStocks() {
 //        System.out.println("123!!!");
-        Optional<Integer> userId = recommendationService.getTopProfitUserId("stock");
+        Optional<Integer> userId = recommendationTopService.getTopProfitUserId("stock");
         if (userId.isPresent()) {
-            return recommendationService.recommendTopStocksForUser(userId.get());
+            return recommendationTopService.recommendTopStocksForUser(userId.get());
         } else {
             // 사용자를 찾지 못했을 경우 빈 리스트 반환
             return List.of();
@@ -35,9 +35,9 @@ public class RecommendationController {
     // 펀드 수익률이 가장 높은 사용자의 상위 3개 펀드 추천
     @GetMapping("/top-funds")
     public List<UserFundResponseDTO> recommendTopFunds() {
-        Optional<Integer> userId = recommendationService.getTopProfitUserId("fund");
+        Optional<Integer> userId = recommendationTopService.getTopProfitUserId("fund");
         if (userId.isPresent()) {
-            return recommendationService.recommendTopFundsForUser(userId.get());
+            return recommendationTopService.recommendTopFundsForUser(userId.get());
         } else {
             // 사용자를 찾지 못했을 경우 빈 리스트 반환
             return List.of();
@@ -47,12 +47,14 @@ public class RecommendationController {
     // 채권 수익률이 가장 높은 사용자의 상위 3개 채권 추천
     @GetMapping("/top-bonds")
     public List<UserBondResponseDTO> recommendTopBonds() {
-        Optional<Integer> userId = recommendationService.getTopProfitUserId("bond");
+        Optional<Integer> userId = recommendationTopService.getTopProfitUserId("bond");
         if (userId.isPresent()) {
-            return recommendationService.recommendTopBondsForUser(userId.get());
+            return recommendationTopService.recommendTopBondsForUser(userId.get());
         } else {
             // 사용자를 찾지 못했을 경우 빈 리스트 반환
             return List.of();
         }
     }
+
+
 }
--- Development/BE/Crunch-it/src/main/java/com/kb/crunchit/service/RecommendationTopService.java
+++ Development/BE/Crunch-it/src/main/java/com/kb/crunchit/service/RecommendationTopService.java
@@ -1,32 +1,59 @@
 package com.kb.crunchit.service;
 
+import com.kb.crunchit.dto.request.StockInfoRequestDto;
 import com.kb.crunchit.dto.response.UserBondResponseDTO;
 import com.kb.crunchit.dto.response.UserFundResponseDTO;
 import com.kb.crunchit.dto.response.UserStockResponseDTO;
+import com.kb.crunchit.dto.response.analysis.StockApiResponse;
+import com.kb.crunchit.dto.response.analysis.StockInfoResponseDto;
 import com.kb.crunchit.mapper.UserAssetStatisticsMapper;
 import com.kb.crunchit.mapper.UserBondMapper;
 import com.kb.crunchit.mapper.UserFundMapper;
 import com.kb.crunchit.mapper.UserStockMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
 
-import java.util.List;
-import java.util.Optional;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 @Service
-public class RecommendationService {
+@RequiredArgsConstructor
+public class RecommendationTopService {
+    private final RedisService redisService;
 
-    @Autowired
-    private UserStockMapper userStockMapper;
+    private final UserStockMapper userStockMapper;
 
-    @Autowired
-    private UserFundMapper userFundMapper;
+    private final UserFundMapper userFundMapper;
 
-    @Autowired
-    private UserBondMapper userBondMapper;
+    private final UserBondMapper userBondMapper;
 
-    @Autowired
-    private UserAssetStatisticsMapper userAssetStatisticsMapper; // user_asset_statistics 매퍼 추가
+    private final UserAssetStatisticsMapper userAssetStatisticsMapper; // user_asset_statistics 매퍼 추가
+
+    @Value("${korea.stock.appkey}")
+    private String appkey;
+
+    @Value("${korea.stock.appsecret}")
+    private String appsecret;
+
+    @Value("${korea.stock.baseUri}")
+    private String baseUri;
+
+    @Value("${korea.stock.grant_type}")
+    private String grant_type;
+
+    // WebClient 생성 시 maxInMemorySize 설정 추가
+    private final WebClient webClient = WebClient.builder()
+            .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(10 * 1024 * 1024))  // 10MB로 설정
+            .build();
 
     // 각 투자 상품에서 수익률이 가장 높은 user_id를 찾는 메서드
     public Optional<Integer> getTopProfitUserId(String type) {
@@ -81,5 +108,29 @@ public class RecommendationService {
                         a.getQuantity() * a.getAveragePurchasePrice()))
                 .limit(3)
                 .toList();
+    }
+
+    // 한국투자증권 토큰 발급
+    public String getKoreaToken() {
+        // Body 설정
+        Map<String, String> body = new HashMap<>();
+        body.put("grant_type", grant_type);
+        body.put("appkey", appkey);
+        body.put("appsecret", appsecret);
+
+        // POST 요청 보내기
+        String responseToken = webClient.post()
+                .uri(baseUri + "/oauth2/tokenP") // 실제 요청할 경로로 설정
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(body)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        System.out.println("응답: " + responseToken);
+
+        redisService.saveKoreaToken("koreaToken", responseToken);
+
+        return responseToken;
     }
 }
