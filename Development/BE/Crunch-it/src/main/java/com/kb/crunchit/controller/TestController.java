1fe823f04eb7736cb85a9ec4b6e8c409425d9051
wonyoung <wjddnjsdud0601@gmail.com>
2024-10-09 22:12:38 +0900

    [Feat]: 한국투자증권 토큰 발급

--- Development/BE/Crunch-it/src/main/java/com/kb/crunchit/controller/TestController.java
+++ Development/BE/Crunch-it/src/main/java/com/kb/crunchit/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.kb.crunchit.controller;
 
 import com.kb.crunchit.security.CustomUserDetails;
+import com.kb.crunchit.service.RecommendationTopService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.SecurityBuilder;
 import org.springframework.security.core.Authentication;
@@ -12,8 +14,11 @@ import java.util.HashMap;
 import java.util.Map;
 // 추후에 삭제 예정
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class TestController {
+    private final RecommendationTopService recommendationTopService;
+
     @GetMapping("/test")
     public ResponseEntity<Map<String,Object>> test(Authentication auth){
         CustomUserDetails user = (CustomUserDetails) auth.getPrincipal();
@@ -25,5 +30,10 @@ public class TestController {
         response.put("nickname", nickname);
         response.put("userId", userId);
         return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("/koreaToken")
+    public ResponseEntity<?> token(){
+        return ResponseEntity.ok().body(recommendationTopService.getKoreaToken());
     }
 }
