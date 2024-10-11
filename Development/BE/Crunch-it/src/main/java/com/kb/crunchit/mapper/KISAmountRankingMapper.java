0dcc1e135e4cafa50ec774dd86b855b090e5ce29
wonyoung <wjddnjsdud0601@gmail.com>
2024-10-11 21:05:18 +0900

    [Feat]: 주식 순위 3개 조회

--- Development/BE/Crunch-it/src/main/java/com/kb/crunchit/mapper/KISAmountRankingMapper.java
+++ Development/BE/Crunch-it/src/main/java/com/kb/crunchit/mapper/KISAmountRankingMapper.java
@@ -1,7 +1,10 @@
 package com.kb.crunchit.mapper;
 
 import com.kb.crunchit.dto.response.StockMarketDataDTO;
+import com.kb.crunchit.entity.KisStockAmountRank;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface KISAmountRankingMapper {
@@ -10,4 +13,6 @@ public interface KISAmountRankingMapper {
     void updateStock(StockMarketDataDTO.StockDataDTO stock);
 
     void insertStock(StockMarketDataDTO.StockDataDTO stock);
+
+    List<KisStockAmountRank> getAllStokAmountRank();
 }
