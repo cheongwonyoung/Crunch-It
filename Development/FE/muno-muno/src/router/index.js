aaf33ae64251948e53cb45d2bd1c6ca9480bf882
Taeyong98 <jang880022@gmail.com>
2024-10-12 14:15:36 +0900

    [FE][FEAT] 온보딩, 로그인 내비게이션 가드 추가

--- Development/FE/muno-muno/src/router/index.js
+++ Development/FE/muno-muno/src/router/index.js
@@ -1,14 +1,14 @@
-import Message from '@/pages/Message.vue';
-import { createRouter, createWebHistory } from 'vue-router';
-import HomeP from '@/pages/Home.vue';
-import ProductP from '@/pages/Product.vue';
-import PropertyP from '@/pages/Property.vue';
-import LoginP from '@/pages/LoginPage.vue';
-import SignupP from '@/pages/SignUpPage.vue';
-import MyPage from '@/pages/MyPage.vue';
-import MyPageEdit from '@/pages/MyPageEdit.vue';
-import Search from '@/pages/Search.vue';
-import Modal from '@/components/Modal.vue';
+import Message from "@/pages/Message.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import HomeP from "@/pages/Home.vue";
+import ProductP from "@/pages/Product.vue";
+import PropertyP from "@/pages/Property.vue";
+import LoginP from "@/pages/LoginPage.vue";
+import SignupP from "@/pages/SignUpPage.vue";
+import MyPage from "@/pages/MyPage.vue";
+import MyPageEdit from "@/pages/MyPageEdit.vue";
+import Search from "@/pages/Search.vue";
+import Modal from "@/components/Modal.vue";
 
 import CommunityP from "@/pages/CommunityPage.vue";
 import PostDetailP from "@/pages/PostDetailPage.vue";
@@ -19,7 +19,7 @@ import MyData from "@/pages/MyData.vue";
 import IncomeSetting from "@/pages/IncomeSetting.vue";
 import LimitSetting from "@/pages/LimitSetting.vue";
 import FindPasswordP from "@/pages/FindPassword.vue";
-// import apiClient from "@/axios";
+import apiClient from "@/axios";
 
 const routes = [
     { path: "/", name: "Home", component: HomeP },
@@ -32,7 +32,7 @@ const routes = [
     { path: "/mypage", name: "MyPage", component: MyPage },
     { path: "/mypageedit", name: "MyPageEdit", component: MyPageEdit },
     { path: "/search", name: "Search", component: Search },
-    { path: "/message/:roomId/:roomName", name: "Message", component: Message, props:true },
+    { path: "/message/:roomId/:roomName", name: "Message", component: Message, props: true },
     { path: "/mydata", name: "MyData", component: MyData },
     { path: "/incomesetting", name: "IncomeSetting", component: IncomeSetting },
     { path: "/limitsetting", name: "LimitSetting", component: LimitSetting },
@@ -44,45 +44,45 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    history: createWebHistory(),
+    routes,
 });
 
-// const navigationRoutes = ["Login", "SignUp"];
-// const navigationOnBoardingRoutes = ["MyData", "IncomeSetting", "LimitSetting"];
+const navigationRoutes = ["Login", "SignUp"];
+const navigationOnBoardingRoutes = ["MyData", "IncomeSetting", "LimitSetting"];
 
-// router.beforeEach(async (to, from, next) => {
-//     const token = localStorage.getItem("JwtToken"); // 토큰을 localStorage에서 가져옴
+router.beforeEach(async (to, from, next) => {
+    const token = localStorage.getItem("JwtToken"); // 토큰을 localStorage에서 가져옴
 
-//     if (!navigationRoutes.includes(to.name)) {
-//         if (!token) {
-//             next({ name: "Login" });
-//             return;
-//         }
-//         const res = await apiClient.get("/auth/authenticate");
-//         try {
-//             if (res.status != 200) next({ name: "Login" });
-//             else {
-//                 if (navigationOnBoardingRoutes.includes(to.name)) {
-//                     next();
-//                 }
-//                 const onBoardingRes = await apiClient.get("/onboarding/check");
-//                 try {
-//                     if (onBoardingRes.data.onBoarded) next();
-//                     else next({ name: "MyData" });
-//                 } catch (err) {
-//                     console.log(err);
-//                     next({ name: "Login" });
-//                 }
-//             }
-//         } catch (err) {
-//             if (err.status == 401) alert("로그인이 만료되었습니다. 다시 로그인 해주세요.");
-//             console.log(err);
-//             next({ name: "Login" });
-//         }
-//     } else {
-//         next();
-//     }
-// });
+    if (!navigationRoutes.includes(to.name)) {
+        if (!token) {
+            next({ name: "Login" });
+            return;
+        }
+        const res = await apiClient.get("/auth/authenticate");
+        try {
+            if (res.status != 200) next({ name: "Login" });
+            else {
+                if (navigationOnBoardingRoutes.includes(to.name)) {
+                    next();
+                }
+                const onBoardingRes = await apiClient.get("/onboarding/check");
+                try {
+                    if (onBoardingRes.data.onBoarded) next();
+                    else next({ name: "MyData" });
+                } catch (err) {
+                    console.log(err);
+                    next({ name: "Login" });
+                }
+            }
+        } catch (err) {
+            if (err.status == 401) alert("로그인이 만료되었습니다. 다시 로그인 해주세요.");
+            console.log(err);
+            next({ name: "Login" });
+        }
+    } else {
+        next();
+    }
+});
 
 export default router;
